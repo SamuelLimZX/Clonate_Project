c5737aae32fe5e1f9714d60c02c98062f2807fcd
SamuelLimZX <samuelkenjilim1997@gmail.com>
2021-05-11 15:08:43 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -32,15 +32,6 @@ Vue.use(VueGoogleMaps, {
 
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-  apiKey: "AIzaSyDtr6cQZ-bMR-dibwF9m6eE3oS9Gc9S3b4",
-  authDomain: "jobit-38c72.firebaseapp.com",
-  projectId: "jobit-38c72",
-  storageBucket: "jobit-38c72.appspot.com",
-  messagingSenderId: "407951507326",
-  appId: "1:407951507326:web:18f29a4c16508568d07d2f",
-  measurementId: "G-W1SZ08LYHH"
-};
 
 firebase.initializeApp(firebaseConfig);
 
